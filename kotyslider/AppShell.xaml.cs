564171cabfa92a94726f33e17ba03a9f527bd37c
Marcin <uT_3777@zsp3zamosc>
2024-11-08 13:45:41 +0100

    Dodaj pliki projektów.

---
+++ kotyslider/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kotyslider
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kotyslider/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace kotyslider
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+
+
+            kot.WidthRequest = 200 * value;
+            kot.HeightRequest = 200 * value;
+        }
+
+
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/says/hello?fontSize=50&fontColor=red";
+
+            kot.Source = link;
+        }
+
+
+    }
+}
