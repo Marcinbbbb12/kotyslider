4b4dd48bfb9703855ee3889d9a8edf9a3307078a
Marcin <uT_3777@zsp3zamosc>
2025-03-04 12:11:44 +0100

    tak

--- kotyslider/MainPage.xaml.cs
+++ kotyslider/MainPage.xaml.cs
@@ -1,31 +1,38 @@
-﻿
-
-namespace kotyslider
+﻿namespace kotyslider
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            UpdateSizeLabel(sizeSlider.Value);
         }
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
             double value = args.NewValue;
 
+            imageToResize.WidthRequest = 200 * value;
+            imageToResize.HeightRequest = 200 * value;
 
-            kot.WidthRequest = 200 * value;
-            kot.HeightRequest = 200 * value;
+            UpdateSizeLabel(value);
         }
 
-
-        private void OnCounterClicked3(object sender, EventArgs e)
+        private void UpdateSizeLabel(double value)
         {
-            string link = "https://cataas.com/cat/says/hello?fontSize=50&fontColor=red";
+            sizeLabel.Text = $"Rozmiar: {value:F2}";
+        }
 
-            kot.Source = link;
+        private void ResetSlider()
+        {
+            sizeSlider.Value = 0.5; 
+            UpdateSizeLabel(sizeSlider.Value); 
         }
 
 
+        private void OnResetButtonClicked(object sender, EventArgs e)
+        {
+            ResetSlider();
+        }
     }
 }
